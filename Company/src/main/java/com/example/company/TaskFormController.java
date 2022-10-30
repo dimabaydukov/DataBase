98278682c1407b1a0a274e9efe2972d195640ba8
Aily-Hhh <aily.hhh13@gmail.com>
2022-10-30 14:56:06 +0300

    update

---
+++ Company/src/main/java/com/example/company/TaskFormController.java
@@ -0,0 +1,16 @@
+package com.example.company;
+
+import javafx.scene.control.*;
+
+public class TaskFormController {
+    public TextField nameTask;
+    public DatePicker dateDeadlineTask;
+    public Spinner priorityTask;
+    public Spinner contractTask;
+    public Spinner employeeTask;
+    public DatePicker dateEndTask;
+    public Label dateCreateTask;
+    public Label statusTask;
+    public Button saveButtonTask;
+    public Button backButton;
+}
---
+++ Company/src/main/java/com/example/company/TaskListFormController.java
@@ -0,0 +1,7 @@
+package com.example.company;
+
+import javafx.scene.control.ListView;
+
+public class TaskListFormController {
+    public ListView listTasks;
+}
